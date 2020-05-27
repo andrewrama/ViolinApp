f3e5932af467f9e0920480567b91d1b201c0a8df
andrewrama <andrewrama27@gmail.com>
2020-05-26 21:32:09 -0400

    Fourth Commit

--- app/src/main/java/andrewrama/honorsmobileapps/violinapp/PracticeLogActivity.java
+++ app/src/main/java/andrewrama/honorsmobileapps/violinapp/PracticeLogActivity.java
@@ -1,11 +1,17 @@
 package andrewrama.honorsmobileapps.violinapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -13,27 +19,73 @@ public class PracticeLogActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private SessionAdapter mAdapter;
+    ArrayList<Session> sessions;
+    private final Context context=this;
+    private static final String KEY_SESSIONS = "sessions";
+    private Bundle bundle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice_log);
 
+        if(Session.getSessionList()!=null) {
+            sessions = Session.getSessionList();
+        }
+        else{
+            sessions = new ArrayList<>();
+        }
         mRecyclerView = findViewById(R.id.logRecyclerView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(layoutManager);
 
-        ArrayList<Session> sessions = new ArrayList<>();
-        sessions.add(new Session("Friday", "4 hours"));
-        sessions.add(new Session("Friday", "4 hours"));
-        sessions.add(new Session("Friday", "4 hours"));
-        sessions.add(new Session("Friday", "4 hours"));
-        sessions.add(new Session("Friday", "4 hours"));
-        sessions.add(new Session("Friday", "4 hours"));
-        sessions.add(new Session("Friday", "4 hours"));
+
         mAdapter = new SessionAdapter(sessions);
         mRecyclerView.setAdapter(mAdapter);
     }
+    public void addNewSession(View v){
+        LayoutInflater li = LayoutInflater.from(PracticeLogActivity.this);
+        View promptView = li.inflate(R.layout.new_session, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(PracticeLogActivity.this);
+        builder.setView(promptView);
+        final EditText sessionTitle = (EditText) promptView.findViewById(R.id.titleEditText);
+        final EditText sessionDescription = (EditText) promptView.findViewById(R.id.notesEditText);
+        builder.setTitle("Create New Session");
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(!sessionTitle.getText().toString().equals("")) {
+                    addToRecyclerView(sessionTitle.getText().toString(), sessionDescription.getText().toString());
+                    dialog.dismiss();
+                }
+                else {
+                    Toast.makeText(context, "Title Required!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+    public void addToRecyclerView(String title, String description){
+        sessions.add(new Session(title, description));
+        mAdapter.notifyDataSetChanged();
+    }
     public void backToHome2(View v){
         finish();
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Session.setSessionList(sessions);
+    }
+    public ArrayList<Session> getSessionsList(){
+        return sessions;
     }
 }
--- app/src/main/java/andrewrama/honorsmobileapps/violinapp/Session.java
+++ app/src/main/java/andrewrama/honorsmobileapps/violinapp/Session.java
@@ -1,14 +1,44 @@
 package andrewrama.honorsmobileapps.violinapp;
 
-public class Session {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+
+public class Session implements Parcelable {
 
     private String mSessionTitle;
     private String mSessionDescription;
+    public static String TYPE = "session";
+    public static ArrayList<Session> sessionList;
 
-    public Session(String sessionTitle, String sessionDescription){
-        mSessionTitle = sessionTitle;
-        mSessionDescription = sessionDescription;
+    public Session(String title, String description){
+        mSessionTitle = title;
+        mSessionDescription = description;
     }
+    public Session(Parcel in){
+        mSessionTitle = in.readString();
+        mSessionDescription = in.readString();
+    }
+
+    @Override
+    public int describeContents(){
+        return 0;
+    }
+
+    public void writeToParcel(Parcel out, int i){
+        out.writeString(mSessionTitle);
+        out.writeString(mSessionDescription);
+    }
+
+    public static final Parcelable.Creator<Session> CREATOR = new Parcelable.Creator<Session>(){
+       public Session createFromParcel(Parcel in){
+           return new Session(in);
+       }
+       public Session[] newArray(int size){
+           return new Session[size];
+       }
+    };
 
     public String getSessionTitle(){
         return mSessionTitle;
@@ -24,6 +54,14 @@ public class Session {
 
     public void setSessionDescription(String sessionDescription){
         mSessionDescription = sessionDescription;
+    }
+
+    public static void setSessionList(ArrayList<Session> list){
+        sessionList = list;
+    }
+
+    public static ArrayList<Session> getSessionList(){
+        return sessionList;
     }
 
 }
